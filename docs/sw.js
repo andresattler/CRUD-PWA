c1534b194fe667a536a343300c5a2df13724dbee
Andre Sattler <andresattler.webdev@gmail.com>
2018-04-18 16:14:26 +0200

    build with mobile optimization

--- docs/sw.js
+++ docs/sw.js
@@ -12,13 +12,13 @@ var __wpo = {
   "externals": [],
   "hashesMap": {
     "4c2e4fc8c6ec0fbfa8f0ece329e5ef5e2337bd50": "./app.js",
-    "1b3f46d7ab7e02d96642e1d2a74bfb8029528895": "./styles.css",
-    "a353c2bd9521f9804101cf3372b26e0132e06340": "./",
+    "ef0d26bf118875d0f004e377130f4b6b6a2d949b": "./styles.css",
+    "67bd7bd39adfe3e2245da45004c36f1b262dfe79": "./",
     "dc45e5c2efef07ea238ebcbfb9f2288dedea6cb2": "./manifest.json"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "18.4.2018, 15:43:47",
+  "version": "18.4.2018, 16:11:32",
   "name": "webpack-offline",
   "pluginVersion": "5.0.0-0",
   "relativePaths": true
